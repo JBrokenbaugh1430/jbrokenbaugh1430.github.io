faebcc77134b7f7cf8a735002306fa15e20221c6
JBrokenbaugh1430 <imadiamondman@gmail.com>
2025-02-09 09:22:44 -0500

    added dark mode support

---
+++ scripts/themes.js
@@ -0,0 +1,13 @@
+if (window.matchMedia("(prefers-color-scheme: dark)").matches){
+    
+    //set background color
+    /*make it a good pratice to comment.
+    once you get a big file, everything gets out of hand
+    */
+    d3.select('body')
+    .style("background-color", "#111111")
+    
+    //select EVERY text element that i'm using and set color to white
+    d3.selectAll("p, h1, h2, h3, h4, h5, h6, a, marquee, small")
+    .style("color", "#ffffff")
+}
