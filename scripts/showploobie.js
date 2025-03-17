57c9e5862781d7ba4d072fd35212a06dfb05e161
JBrokenbaugh <79676090+JBrokenbaugh1430@users.noreply.github.com>
2025-03-17 06:52:03 -0400

    Update.js

--- scripts/showploobie.js
+++ scripts/showploobie.js
@@ -1,16 +1,2 @@
-const Button = document.getElementById("PloobieButton")
-function ShowPloobie(){
 
-    const Ploobie = document.getElementById("PloobieImg")
-    Ploobie.src = "https://raw.githubusercontent.com/JBrokenbaugh1430/sharing/refs/heads/main/ploobie.png"
-    Ploobie.width = 755
-    Ploobie.height = 743
 
-    setTimeout(() => {
-        
-        Ploobie.width = 0
-    Ploobie.height = 0
-    }, 500);
-}   
-
-Button.onclick = ShowPloobie
