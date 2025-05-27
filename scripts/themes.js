66c42bbf3cd11d809e765e7cac5743cbac0b6344
JBrokenbaugh <79676090+JBrokenbaugh1430@users.noreply.github.com>
2025-05-26 21:00:17 -0400

    Update themes.js

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,14 +1,11 @@
 if (window.matchMedia("(prefers-color-scheme: dark)").matches){
     
     
-    /*make it a good pratice to comment.
-    once you get a big file, everything gets out of hand
-    */
    //set background color
     d3.select('body')
     .style("background-color", "#111111")
     
-    //select EVERY text element that i'm using and set color to white
-    d3.selectAll("p, h1, h2, h3, h4, h5, h6, a, marquee, small")
+    //select every text element that i'm using and set color to white
+    d3.selectAll("p, h1, h2, h3, h4, h5, h6, a, marquee, small, li")
     .style("color", "#ffffff")
 }
